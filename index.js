976837a571c199163b3d06181817e61ed2051a0d
fadilhakim <fadil.nylon@gmail.com>
2018-02-15 18:20:49 +0800

    add assets

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(__dirname + '/public'));
+
+// views
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+// models
+
+
+// routes -> controller
+
+const home = require('./routes/home')
+const benefits = require('./routes/benefits')
+
+app.use('/',home)
+app.use('/benefits',benefits)
+
 
 app.listen(3000, () => console.log('App listening on port 3000!'))
