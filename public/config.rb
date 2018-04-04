077b91764a20011dfeb776dd92999a5a656c0c7f
Arthur Pello <arthurnumen12@gmail.com>
2018-04-04 13:55:20 +0700

    Test assets

--- public/config.rb
+++ public/config.rb
@@ -1,9 +1,14 @@
 require 'compass/import-once/activate';
+
+Encoding.default_external = "utf-8"
+
+http_path = "https://london.bridestory.com"
+
+http_images_dir = "raw/upload/business-home/assets/"
 #require "compass/utilities/sprites";
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
 css_dir = "stylesheets"
 sass_dir = "sass"
 images_dir = "assets"
@@ -11,6 +16,9 @@ javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 output_style = :compact 
+
+# relative_assets = true # For working on local
+relative_assets = false # For working on staging & production
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
@@ -24,3 +32,5 @@ output_style = :compact
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+asset_cache_buster :none
