a1fba601d74acebabddb2bfc8da14b83508fa827
adityahilman <aditya.hilman@bridestory.com>
2018-04-05 11:52:25 +0700

    container port in testing script

--- container-testing.sh
+++ container-testing.sh
@@ -1,8 +1,8 @@
 docker run --rm --name vendor-home-staging -d -P 172405484086.dkr.ecr.ap-southeast-1.amazonaws.com/vendor-home:staging-${BUILD_NUMBER}
-PAYMENT_IP=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' vendor-home-staging`
+VENDOR_IP=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' vendor-home-staging`
 ENDPOINT_URL="home"
 sleep 30s
-RESPONSE_CODE=`curl -I -X GET http://$PAYMENT_IP/$ENDPOINT_URL | head -n1 | awk '{ print $2 }'`
+RESPONSE_CODE=`curl -I -X GET http://$VENDOR_IP:3000/$ENDPOINT_URL | head -n1 | awk '{ print $2 }'`
 if [[ $RESPONSE_CODE != "200" ]]
 then
 	echo "Tests Failed! - Return code $RESPONSE_CODE"
