5d1d571b8699eb5f4ac0300e746c9f973fd39fca
fadilhakim <fadil.nylon@gmail.com>
2018-04-18 10:13:31 +0800

    reset lang

--- middleware/langChecker.js
+++ middleware/langChecker.js
@@ -1,17 +1,20 @@
 module.exports = langChecker
 
-function langChecker(req, res, next) {
-    if (req.headers.country) {
-        var lang = req.headers.country.toLowerCase();
 
-        if (lang == 'id') {
-            res.redirect('/' + lang + req.originalUrl);
-        } else {
-            next();
-        }
+function langChecker(req, res, next) {
+
+    if (req.params.lang) {
+        next();
     } else {
-        if(req.cookies.BS_PreferredLang && req.cookies.BS_PreferredLang == 'id') {
-            res.redirect('/' + lang + req.originalUrl);
+        if (req.headers.country) {
+            var lang = req.headers.country.toLowerCase();
+
+            if (lang == 'id') {
+                res.redirect('/' + lang + req.originalUrl);
+            } else {
+                next();
+            }
+
         } else {
             next();
         }
