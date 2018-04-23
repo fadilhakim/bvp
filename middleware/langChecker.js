b4c4178519b2a8791fe424e8b30ab7d6d4038034
Yusuf Adam <yusufadam1207@gmail.com>
2018-04-23 17:56:29 +0700

    test get country

--- middleware/langChecker.js
+++ middleware/langChecker.js
@@ -3,9 +3,9 @@ module.exports = langChecker
 
 function langChecker(req, res, next) {
 
-    if (req.headers.country) {
+    if (req.get('CloudFront-Viewer-Country')) {
         var lang = req.cookies.BS_PreferredLang ? req.cookies.BS_PreferredLang : req.headers.country.toLowerCase();
-        if(req.headers.country.toLowerCase() == 'ph' || req.headers.country.toLowerCase() == 'sg' || req.headers.country.toLowerCase() == 'id') 
+        if(req.get('CloudFront-Viewer-Country').toLowerCase() == 'ph' || req.get('CloudFront-Viewer-Country').toLowerCase() == 'sg' || req.get('CloudFront-Viewer-Country').toLowerCase() == 'id') 
             var country = req.headers.country.toLowerCase();
         else 
             var country = 'en';
